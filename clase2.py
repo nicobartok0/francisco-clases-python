5e2fb2e3db30ccb7d1c1bf5b54f2d7357fa038cf
nicobartok0 <nicobartok0@gmail.com>
2024-07-22 17:36:13 -0300

    Clase del Lunes 22/07

---
+++ clase2.py
@@ -0,0 +1,36 @@
+# Bucles FOR
+variable = True
+lista = [4, "papa", 3, 5, 2]
+
+
+print(range(5,10,2)) # -- Función RANGE devuelve objeto range (start, stop, step)
+
+for i in range(5, 10, 2): # -- Función for con un rango RANGE
+    print(i)
+
+print('\n\n')
+
+for element in lista: # -- Bucle for iterando elementos de una lista
+    print(element)
+
+while variable: # -- While variable booleana 
+    print('Hola infinito')    
+    for i in range(5, 10, 2): # -- Función for con un rango RANGE
+        print(i)
+        if i == 9:
+            variable = False
+
+print('\n\n')
+
+
+# Listas y matrices
+
+matriz = [
+    [1, 0, 0],
+    [0, 1, 0],
+    [0, 0, 1]
+] # Matriz identidad
+
+print(matriz) # Imprime la matriz
+print(matriz[0]) # Imprime la primer fila de la matriz
+print(matriz[0][0]) # Imprime el primer valor de la primer fila de la matriz
